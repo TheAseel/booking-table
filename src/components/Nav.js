5396b71f3922d5fd810e2900e8028b4bc548d853
Aseel <Aseelmaha29@gmail.com>
2024-05-20 22:28:32 +0300

    frist nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from "../imgs/Logo .svg"
 
-function Nav() {
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="Nav">
-<img src="/logo192.png" alt="Logo" />
- <nav>
-      <ul>
+    <nav className={`navbar ${isOpen ? "open" : ""}`}>
+      <img src={logo} alt="Logo" />
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "visible" : ""}`}>
         <li><a href="/">Home</a></li>
         <li><a href="/About">About</a></li>
         <li><a href="/Menu">Menu</a></li>
         <li><a href="/Reservation">Reservation</a></li>
         <li><a href="/Order Online">Order Online</a></li>
         <li><a href="/Login">Login</a></li>
-        
       </ul>
     </nav>
-  </div>
-    
   );
 }
 
